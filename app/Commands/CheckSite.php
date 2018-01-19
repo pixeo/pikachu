d7fba1a10619f635507f0142126ea63e07346fab
Nick Goris <nick@pixeo.be>
2018-01-19 08:18:19 +0100

    refactor CheckSite

--- app/Commands/CheckSite.php
+++ app/Commands/CheckSite.php
@@ -5,6 +5,7 @@ namespace App\Commands;
 use App\Services\Checker;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Uri;
+use Illuminate\Support\Collection;
 use LaravelZero\Framework\Commands\Command;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -41,39 +42,49 @@ class CheckSite extends Command
      */
     public function handle(): void
     {
+        $this->getLocations()
+            ->each(function ($location) {
+                $path = (new Uri($location->textContent))->getPath();
+
+                $results = collect(app(Checker::class)->validate($location->textContent));
+
+                $this->info($this->buildMessage(
+                    $path,
+                    $results->where('passed', true)->count(),
+                    $results->where('passed', false)->count()
+                ));
+
+                $this->output->writeln("");
+
+                $results->where('passed', false)->each(function ($result) {
+                    $this->warn("\t" . $result['message']);
+                });
+
+                $this->output->writeln("");
+                $this->output->writeln("");
+            });
+    }
+
+    /**
+     * @return Collection
+     */
+    protected function getLocations(): Collection
+    {
         $client = new Client();
         $response = $client->request('GET', $this->argument('sitemap'));
 
         $crawler = new Crawler($response->getBody()->getContents());
-        $locations = $crawler->filterXPath('//loc');
+        return collect($crawler->filterXPath('//loc'));
+    }
 
-        $maxLength = collect($locations)->max(function ($location) {
-            return strlen((new Uri($location->textContent))->getPath());
-        });
-
-        foreach ($locations as $location) {
-            $path = (new Uri($location->textContent))->getPath();
-
-            $results = collect(app(Checker::class)->validate($location->textContent));
-
-            $message = sprintf(
-                "%s%s\xf0\x9f\x91\x8d  %d | \xF0\x9F\x91\x8E  %d",
-                $path,
-                str_repeat(" ", ($maxLength - strlen($path)) + 5),
-                $results->where('passed', true)->count(),
-                $results->where('passed', false)->count()
-            );
-
-            $this->info($message);
-            $this->output->writeln("");
-
-            $results->where('passed', false)->each(function ($result) {
-                $this->warn("\t" . $result['message']);
-            });
-
-            $this->output->writeln("");
-            $this->output->writeln("");
-        }
-
+    /**
+     * @param $path
+     * @param $passed
+     * @param $failed
+     * @return string
+     */
+    protected function buildMessage($path, $passed, $failed): string
+    {
+        return sprintf("\xf0\x9f\x91\x8d  %d | \xF0\x9F\x91\x8E  %d\t%s", $passed, $failed, $path);
     }
 }
